8cf4850f1e7f652549207f4cd1a0c61445273393
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-22 20:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function majorityElementChecker(arr)
+    {
+        let map = new Map();
+ 
+        for(let i = 0; i < arr.length; i++) {
+            if (map.has(arr[i])) {
+                    let count = map.get(arr[i]) +1;
+                    if (count > arr.length /2) {
+                        document.write("Majority found :- " + arr[i]);
+                        return;
+                    } else
+                        map.set(arr[i], count);
+ 
+            }
+            else
+                map.set(arr[i],1);
+            }
+            document.write(" No Majority element");
+    }
+majorityElementChecker()
